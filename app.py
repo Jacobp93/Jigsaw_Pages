c654b7da471a8c7cb07181fb79c4dd29defdeed2
Jacob <jacobp1997@live.co.uk>
2024-11-25 15:48:21 +0000

    Master

--- app.py
+++ app.py
@@ -369,9 +369,9 @@ WHERE (property_customer_type IS NOT NULL AND property_customer_type != '')
 
 # Subplots for Total SaaS and Total Legacy by Region with PSHE and RE breakdowns
     fig = make_subplots(
-    rows=1, cols=2,
+    rows=2, cols=2,
     subplot_titles=(
-         "RE SaaS vs RE Legacy" ,"PSHE SaaS vs PSHE Legacy", 
+        "PSHE SaaS vs PSHE Legacy", "RE SaaS vs RE Legacy Breakdown"
     )
     )
 
